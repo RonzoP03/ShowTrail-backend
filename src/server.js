6a889a93796947141a83ea442bc6a0752b6198c0
Ronzo <ronzo@RONALDOP-MBP.local>
2022-04-10 17:35:59 +0200

    Restructure folders for netlify

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
+const serverless = require('serverless-http')
+
 dotenv.config();
 
 connectDB();
@@ -13,17 +15,10 @@ const app = express(); // main thing
 
 app.use(express.json()); // to accept json data
 
-app.use("/api/users", userRoutes);
+app.use("/.netifly/functions/api/users", userRoutes);
 
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 8080;
-
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
-  )
-);
+module.exports.handler = serverless(app)
