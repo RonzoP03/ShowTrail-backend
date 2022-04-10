e657dba41b69d78f7a753e767356d26ab7463f68
Ronzo <ronzo@RONALDOP-MBP.local>
2022-04-10 16:55:20 +0200

    User endpoint implementation

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {
+  authUser,
+  registerUser
+} from "../controllers/userController.js";
+const router = express.Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(authUser);
+
+export default router;
